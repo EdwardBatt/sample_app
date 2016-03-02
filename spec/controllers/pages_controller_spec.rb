2860011884342edc9884dc83ac37a8c6529cbaa2
Edward Batt <edwardbatt@gmail.com>
2016-03-02 11:14:26 +0700

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should contain("title, Ruby on Rails Tutorial Sample App | Home")
+
     end
   end
 
@@ -13,6 +20,24 @@ RSpec.describe PagesController, type: :controller do
     it "returns http success" do
       get :contact
       expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should contain("title, Ruby on Rails Tutorial Sample App | Contact")
+
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should contain("title, Ruby on Rails Tutorial Sample App | About")
     end
   end
 
